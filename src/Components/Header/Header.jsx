c37a85b80f17dee01796cdfdff3fbf3ca182f443
Shivam Gupta <er.shivamsg@gmail.com>
2024-03-06 15:07:12 +0530

    final code readey to deploy

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import logo from "../../assets/logos/medi-logo.png";
+import { Outlet, Link } from "react-router-dom";
+import "./Header.css";
+
+function Header() {
+  const scrollToSection = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+  let rbndata = "Contact Us: info@medicaps.ac.in | Call: 079690-24444";
+  return (
+    <>
+      <div className="full-header">
+        <div className="ribbon-top">
+          {/* <marquee className="marquee" behavior="scroll" direction="left"> */}
+          {rbndata}
+          {/* </marquee> */}
+        </div>
+        <div className="my-header">
+          <nav
+            className="navbar navbar-expand-lg navbar-light bg-light"
+            style={{ marginTop: "-16px" }}
+          >
+            <div className="container-fluid">
+              <a className="navbar-brand" href="/">
+                <img src={logo} className="medi-logo" alt="medi-logo" />
+              </a>
+              <button
+                className="navbar-toggler "
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+                style={{ paddingLeft: "6px", paddingRight: "6px" }}
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <Link className="nav-link active" aria-current="page">
+                      <a onClick={() => scrollToSection("why-mu")}>
+                        Why MU-SAT
+                      </a>
+                    </Link>
+                  </li>
+
+                  <li className="nav-item">
+                    <Link className="nav-link">
+                      <a onClick={() => scrollToSection("carrer")}>
+                        Career Opportunities
+                      </a>
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link">
+                      <a onClick={() => scrollToSection("programs")}>
+                        Programs
+                      </a>
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link">
+                      <a onClick={() => scrollToSection("lifeatmu")}>
+                        Life @ MU
+                      </a>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+        <Outlet />
+      </div>
+    </>
+  );
+}
+
+export default Header;
